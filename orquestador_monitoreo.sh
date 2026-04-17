8a68f53adb9d22dba1c04613c9196c944a70763e
mfigueroaas <mfigueroaas@utem.cl>
2026-04-17 10:27:07 -0400

    Cambios en el orquestador para ejecutar los nuevos scripts

--- orquestador_monitoreo.sh
+++ orquestador_monitoreo.sh
@@ -1,40 +1,32 @@
 #!/bin/bash
-# =========================================================
-# Script Lanzador para el Host LXC (Proxmox)
-# =========================================================
-
-# Esta variable detecta automáticamente en qué carpeta clonaste el repo
-REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
-
+REPO_DIR="/root/monitoreo_lab"
 echo "=== INICIANDO MONITOREO GENERAL - $(date) ==="
 
-echo "-> Fase 1: Escaneando iDRAC..."
-docker run --rm \
-  --privileged \
-  --cap-add=NET_ADMIN \
-  --dns=8.8.8.8 \
-  --device=/dev/net/tun \
-  --device=/dev/ppp \
-  --env-file "$REPO_DIR/data/.env" \
-  -v "$REPO_DIR/data:/app/data" \
-  -v /etc/localtime:/etc/localtime:ro \
-  -e SCRIPT_A_EJECUTAR="script_idrac.py" \
-  monitor-vpn
+# FASE 1: XClarity (Hardware Físico)
+docker run --rm --privileged --cap-add=NET_ADMIN --dns=8.8.8.8 \
+  --device=/dev/net/tun --device=/dev/ppp \
+  --env-file "$REPO_DIR/data/.env" -v "$REPO_DIR/data:/app/data" \
+  -v /etc/localtime:/etc/localtime:ro -e TZ=America/Santiago \
+  -e SCRIPT_A_EJECUTAR="script.py" monitor-vpn
 
-echo "-> Fase 1 Terminada. Dando 15 segundos al Firewall para limpiar la sesion VPN..."
+echo "-> Fase 1 Terminada. Esperando 15s..."
 sleep 15
 
-echo "-> Fase 2: Escaneando ESXi directo..."
-docker run --rm \
-  --privileged \
-  --cap-add=NET_ADMIN \
-  --dns=8.8.8.8 \
-  --device=/dev/net/tun \
-  --device=/dev/ppp \
-  --env-file "$REPO_DIR/data/.env" \
-  -v "$REPO_DIR/data:/app/data" \
-  -v /etc/localtime:/etc/localtime:ro \
-  -e SCRIPT_A_EJECUTAR="script_esxi.py" \
-  monitor-vpn
+# FASE 2: vCenter (Entorno Virtualizado)
+docker run --rm --privileged --cap-add=NET_ADMIN --dns=8.8.8.8 \
+  --device=/dev/net/tun --device=/dev/ppp \
+  --env-file "$REPO_DIR/data/.env" -v "$REPO_DIR/data:/app/data" \
+  -v /etc/localtime:/etc/localtime:ro -e TZ=America/Santiago \
+  -e SCRIPT_A_EJECUTAR="script_vcenter.py" monitor-vpn
 
-echo "=== FIN DEL MONITOREO ==="
+echo "-> Fase 2 Terminada. Esperando 15s..."
+sleep 15
+
+# FASE 3: ESXi Directo (Gestión de Hosts)
+docker run --rm --privileged --cap-add=NET_ADMIN --dns=8.8.8.8 \
+  --device=/dev/net/tun --device=/dev/ppp \
+  --env-file "$REPO_DIR/data/.env" -v "$REPO_DIR/data:/app/data" \
+  -v /etc/localtime:/etc/localtime:ro -e TZ=America/Santiago \
+  -e SCRIPT_A_EJECUTAR="script_esxi.py" monitor-vpn
+
+echo "=== CICLO DE MONITOREO FINALIZADO ==="
